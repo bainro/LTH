fbc36d704b69aa5812c7955087331bbba262b2e0
Robert Bain <31903812+bainro@users.noreply.github.com>
2021-12-24 14:34:50 -0800

    Create noise.py

---
+++ noise.py
@@ -0,0 +1,31 @@
+"""
+script for testing the noise robustness of ~Lenet5 WLTs & RLTs on MNIST & CIFAR10
+"""
+import subprocess
+import time
+
+def check_for_done(l):
+    for i, p in enumerate(l):
+        if p.poll() is not None:
+            return True, i
+    return False, False
+
+processes = list()
+N = 8
+queue = list()
+for process in queue:
+    p = subprocess.Popen(process)
+    processes.append(p)
+    if len(processes) == N:
+        wait = True
+        while wait:
+            done, num = check_for_done(processes)
+
+            if done:
+                # need to check gpu on process cmd call
+                finished_p = processes.pop(num)
+                print("finished_p: ", finished_p)
+                wait = False
+            else:
+                # set so the cpu can save cycles checking
+                time.sleep(0.5)
