42146809ac9e4c18f760947605d1f787960a8b05
Robert Bain <31903812+bainro@users.noreply.github.com>
2021-12-24 23:37:23 -0800

    Update noise.py

--- noise.py
+++ noise.py
@@ -18,16 +18,16 @@ q_l = []
 for trial in range(n_repeats):
     exp_name = "trial_" + str(trial)
     cmd = "python main.py --dataset cifar10 --arch_type lenet5 --end_iter 35 --last_iter_epochs 35 --batch_size 200 --exp_name "
-    cmd += exp_name
+    cmd += "wlt_" + exp_name
     q_l.append(['/bin/bash', '-c', cmd])
     cmd = "python main.py --dataset mnist --arch_type lenet5 --end_iter 35 --last_iter_epochs 35 --batch_size 200 --exp_name "
-    cmd += exp_name
+    cmd += "wlt_" + exp_name
     q_l.append(['/bin/bash', '-c', cmd])
     cmd = "python main.py --dataset cifar --arch_type lenet5 --end_iter 35 --last_iter_epochs 35 --batch_size 200 --rlt --exp_name "
-    cmd += exp_name
+    cmd += "rlt_" + exp_name
     q_l.append(['/bin/bash', '-c', cmd])
     cmd = "python main.py --dataset mnist --arch_type lenet5 --end_iter 35 --last_iter_epochs 35 --batch_size 200 --rlt --exp_name "
-    cmd += exp_name
+    cmd += "rlt_" + exp_name
     q_l.append(['/bin/bash', '-c', cmd])
 
 done_i = None
