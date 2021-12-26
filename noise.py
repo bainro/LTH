09c21281405953ee785a767ca3bb5992316b9367
Robert Bain <31903812+bainro@users.noreply.github.com>
2021-12-26 12:01:08 -0800

    Update noise.py

--- noise.py
+++ noise.py
@@ -2,7 +2,12 @@
 script for testing the noise robustness of ~Lenet5 WLTs & RLTs on MNIST & CIFAR10 
 """
 from subprocess import Popen, PIPE
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import seaborn as sns
+import numpy as np
 import time
+import os
 
 def check_for_done(l):
     for i, (_i, p) in enumerate(l):
@@ -15,6 +20,7 @@ N = 6
 n_repeats = 10
 q_l = []
 
+'''
 for trial in range(n_repeats):
     exp_name = "trial_" + str(trial)
     cmd = "python main.py --dataset cifar10 --arch_type fc1 --end_iter 35 --last_iter_epochs 35 --batch_size 200 --exp_name "
@@ -29,6 +35,7 @@ for trial in range(n_repeats):
     cmd = "python main.py --dataset mnist --arch_type fc1 --end_iter 35 --last_iter_epochs 35 --batch_size 200 --rlt --exp_name "
     cmd += "rlt_" + exp_name
     q_l.append(['/bin/bash', '-c', cmd])
+'''
 
 # keeps 2 training threads per gpu
 done_i = None
@@ -61,7 +68,32 @@ for i, process in enumerate(q_l):
                 # set so the cpu can chill
                 time.sleep(0.5)
                 
-# avg of 10 for sparsity vs acc with RLT + WLT on MNIST + CIFAR10
+# avg of 10 tickets. Sparsity vs acc with WLT + RLT on MNIST + CIFAR10
+DPI = 1200
+prune_iterations = 35
+
+dump_dir = f"{os.getcwd()}/dumps/lt/{arch_type}/{dataset}/"
+for arch_type in tqdm(arch_types):
+    # loads 1 of 40 trials... Need to avg over 10 similar for this dataset 
+    _RENAME = os.path.join(dump_dir, "rlt_" + exp_name, "lt_bestaccuracy.dat")
+    d = np.load(_RENAME, allow_pickle=True)
+    b = np.load(f"{os.getcwd()}/dumps/lt/{arch_type}/{dataset}/lt_bestaccuracy.dat", allow_pickle=True)
+    c = np.load(f"{os.getcwd()}/dumps/lt/{arch_type}/{dataset}/reinit_bestaccuracy.dat", allow_pickle=True)
+
+    a = np.arange(prune_iterations)
+    plt.plot(a, b, c="blue", label="Winning tickets") 
+    plt.plot(a, c, c="red", label="Random tickets") 
+    plt.title(f"Test Accuracy vs Weights % ({arch_type} | {dataset})") 
+    plt.xlabel("Weights %") 
+    plt.ylabel("Test accuracy") 
+    plt.xticks(a, d, rotation ="vertical") 
+    plt.ylim(0,100)
+    plt.legend() 
+    plt.grid(color="gray") 
+
+    plt.savefig(f"{os.getcwd()}/plots/lt/combined_plots/combined_{arch_type}_{dataset}.png", dpi=DPI, bbox_inches='tight') 
+    plt.close()
+        
 # load each checkpoint & do noisy testing
     # multiple types of noise
     # sparsity vs noise score for RLT + WLT on MNIST + CIFAR10
