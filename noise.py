57f768a09c41493a881f722306dafc4ea4d598c1
Robert Bain <31903812+bainro@users.noreply.github.com>
2021-12-29 10:05:41 -0800

    Update noise.py

--- noise.py
+++ noise.py
@@ -145,10 +145,10 @@ for noise_type in [0]: #range(4):
             # WLT ~70% pruned
             # model = torch.load(f"/home/rbain/git/LTH2/saves/lenet5/mnist/wlt_trial_{trial}/9_model_lt.pth.tar")
             # RLT ~70% pruned
-            model = torch.load(f"/home/rbain/git/LTH2/saves/lenet5/mnist/rlt_trial_{trial}/9_model_lt.pth.tar")
+            # model = torch.load(f"/home/rbain/git/LTH2/saves/lenet5/mnist/rlt_trial_{trial}/9_model_lt.pth.tar")
             
             # WLT 96.7% pruned
-            # model = torch.load(f"/home/rbain/git/LTH2/saves/lenet5/mnist/wlt_trial_{trial}/26_model_lt.pth.tar")
+            model = torch.load(f"/home/rbain/git/LTH2/saves/lenet5/mnist/wlt_trial_{trial}/26_model_lt.pth.tar")
             # RLT 96.7% pruned
             # model = torch.load(f"/home/rbain/git/LTH2/saves/lenet5/mnist/rlt_trial_{trial}/26_model_lt.pth.tar")
             
