9a725c69d4dd84eeb25f1b62e8c08a7235ce377f
Robert Bain <31903812+bainro@users.noreply.github.com>
2021-12-29 09:39:52 -0800

    Update noise.py

--- noise.py
+++ noise.py
@@ -72,7 +72,7 @@ for i, process in enumerate(q_l):
                 time.sleep(0.5)
         
 # avg of n_repeat (eg 8) tickets. Sparsity vs acc with WLT + RLT on MNIST + CIFAR10
-for dataset in ["mnist"]:
+for dataset in []:
     dump_dir = f"{os.getcwd()}/dumps/lt/lenet5/{dataset}/"
     
     a = np.arange(prune_iterations)
@@ -118,10 +118,6 @@ for dataset in ["mnist"]:
     plt.close()
         
 # load each checkpoint & do noisy testing
-    # multiple types of noise
-    # sparsity vs noise score for RLT + WLT on MNIST + CIFAR10
-
-exit()
 for noise_type in [0]: #range(4):
     print("===============")
     print("noise_type: ", noise_type)
@@ -129,38 +125,34 @@ for noise_type in [0]: #range(4):
         print("noise_lvl: ", noise_lvl)
         trial_sum = 0
         for trial in range(n_repeats):
-            logdir = f"{os.getcwd()}/plots/mnist_{noise_type}_{noise_lvl}_{trial}/"
+            logdir = f"{os.getcwd()}/plots/cifar10_{noise_type}_{noise_lvl}_{trial}/"
             exists = os.path.exists(logdir)
             if not exists:
                   os.makedirs(logdir)
-            _traindata, testdata = get_split("mnist", noise_type=noise_type, noise_lvl=noise_lvl, logdir=logdir)
+            #_traindata, testdata = get_split("cifar10", noise_type=noise_type, noise_lvl=noise_lvl, logdir=logdir)
             # less noise_lvl needed for cifar10 to be a similar classification difficulty for human annotators (i.e. me!).
-            #_traindata, testdata = get_split("cifar10", noise_type=3, noise_lvl=0.5/3)
+            _traindata, testdata = get_split("cifar10", noise_type=noise_type, noise_lvl=noise_lvl/3)
             test_loader = torch.utils.data.DataLoader(testdata, batch_size=512, shuffle=False, num_workers=2, drop_last=False)
 
             # 100% dense
-            # model = torch.load(f"/home/rbain/git/LTH2/saves/fc1/mnist/wlt_trial_{trial}/0_model_lt.pth.tar")
+            model = torch.load(f"/home/rbain/git/LTH2/saves/fc1/cifar10/wlt_trial_{trial}/0_model_lt.pth.tar")
             
             # WLT ~80% pruned
-            # model = torch.load(f"/home/rbain/git/LTH2/saves/fc1/mnist/wlt_trial_{trial}/12_model_lt.pth.tar")
+            # model = torch.load(f"/home/rbain/git/LTH2/saves/fc1/cifar10/wlt_trial_{trial}/12_model_lt.pth.tar")
             # RLT ~80% pruned
-            # model = torch.load(f"/home/rbain/git/LTH2/saves/fc1/mnist/rlt_trial_{trial}/12_model_lt.pth.tar")
+            # model = torch.load(f"/home/rbain/git/LTH2/saves/fc1/cifar10/rlt_trial_{trial}/12_model_lt.pth.tar")
 
             # WLT ~70% pruned
-            # model = torch.load(f"/home/rbain/git/LTH2/saves/fc1/mnist/wlt_trial_{trial}/9_model_lt.pth.tar")
+            # model = torch.load(f"/home/rbain/git/LTH2/saves/fc1/cifar10/wlt_trial_{trial}/9_model_lt.pth.tar")
             # RLT ~70% pruned
-            model = torch.load(f"/home/rbain/git/LTH2/saves/fc1/mnist/rlt_trial_{trial}/9_model_lt.pth.tar")
+            # model = torch.load(f"/home/rbain/git/LTH2/saves/fc1/cifar10/rlt_trial_{trial}/9_model_lt.pth.tar")
             
             # WLT 96.7% pruned
-            # model = torch.load(f"/home/rbain/git/LTH2/saves/fc1/mnist/wlt_trial_{trial}/26_model_lt.pth.tar")
+            # model = torch.load(f"/home/rbain/git/LTH2/saves/fc1/cifar10/wlt_trial_{trial}/26_model_lt.pth.tar")
             # RLT 96.7% pruned
-            # model = torch.load(f"/home/rbain/git/LTH2/saves/fc1/mnist/rlt_trial_{trial}/26_model_lt.pth.tar")
+            # model = torch.load(f"/home/rbain/git/LTH2/saves/fc1/cifar10/rlt_trial_{trial}/26_model_lt.pth.tar")
             
             trial_sum += test(model, test_loader)
             
         avg_acc = trial_sum / n_repeats
         print("avg trial acc: ", avg_acc)
-    # save 
-    
-# (separate script) train single WLT & check for overfitting
-    # will require spliting training into ""+ validation set
