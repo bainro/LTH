86c627ecd182a601c85a81f3cff2d842241e938f
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-05-15 17:29:03 -0700

    Update vis_noise.py

--- vis_noise.py
+++ vis_noise.py
@@ -8,16 +8,15 @@ import torch
 import os
 
 datasets = ["mnist", "cifar10"]
+noise_lvls = [0, 0.1, 0.2, 0.3, 0.4, 0.5]
+# noise type is randomly set pixels to 2 * st. dev. px value wrt whole image
+two_sd = 0
 
-# load each checkpoint & do noisy testing
-for noise_type in [0]: #range(4):
-    print("===============")
-    print("noise_type: ", noise_type)
-    for noise_lvl in [0, 0.1, 0.2, 0.3, 0.4, 0.5]:
-        print("noise_lvl: ", noise_lvl)
-        _traindata, testdata = get_split("mnist", noise_type=noise_type, noise_lvl=noise_lvl, log>
-        # less noise_lvl needed for cifar10 to be a similar classification difficulty for human a>
-        #_traindata, testdata = get_split("cifar10", noise_type=noise_type, noise_lvl=noise_lvl/3)
-        test_loader = torch.utils.data.DataLoader(testdata, batch_size=512, shuffle=False, num_wo>
-        trial_sum += test(model, test_loader)
+for noise_lvl in noise_lvls:
+    print("noise_lvl: ", noise_lvl)
+    _traindata, testdata = get_split("mnist", noise_type=two_sd, noise_lvl=noise_lvl, log>
+    # less noise_lvl needed for cifar10 to be a similar classification difficulty for human a>
+    #_traindata, testdata = get_split("cifar10", noise_type=noise_type, noise_lvl=noise_lvl/3)
+    test_loader = torch.utils.data.DataLoader(testdata, batch_size=512, shuffle=False, num_wo>
+    trial_sum += test(model, test_loader)
 
