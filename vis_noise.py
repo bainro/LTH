a95823b8a115f395839db6b399970b7db01b3313
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-05-15 19:01:59 -0700

    Update vis_noise.py

--- vis_noise.py
+++ vis_noise.py
@@ -14,14 +14,12 @@ noise_lvls = [0, 0.1, 0.2, 0.3, 0.4, 0.5]
 two_sd = 0
 
 for i, noise_lvl in enumerate(noise_lvls):
-    _, test_data = get_split("mnist", noise_type=two_sd, noise_lvl=noise_lvl)
+    # _, test_data = get_split("mnist", noise_type=two_sd, noise_lvl=noise_lvl)
     
     # less noise_lvl needed for cifar10 to be a similar classification difficulty for human a>
-    # _, test_data = get_split("cifar10", noise_type=two_sd, noise_lvl=noise_lvl/3)
-    test_loader = torch.utils.data.DataLoader(test_data, batch_size=1, shuffle=False, num_workers=2, drop_last=False)
+    _, test_data = get_split("cifar10", noise_type=two_sd, noise_lvl=noise_lvl/3)
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size=1, shuffle=False)
                              
     for _, (img, target) in enumerate(test_loader):
-        print("target", target)
-        print("img tensor shape", img.shape)
         save_image(img, f"mnist_{i}.png")
         if _ == 1: break
