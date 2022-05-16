ab24287206b67286008e7275d5d21ffce8f79961
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-05-15 19:25:44 -0700

    Update vis_noise.py

--- vis_noise.py
+++ vis_noise.py
@@ -14,10 +14,6 @@ noise_lvls = [0, 0.1, 0.2, 0.3, 0.4, 0.5]
 two_sd = 0
 
 for i, noise_lvl in enumerate(noise_lvls):
-    if i != 10 and i != 29: 
-        continue
-    if i > 29: 
-        break
     # _, test_data = get_split("mnist", noise_type=two_sd, noise_lvl=noise_lvl)
     
     # less noise_lvl needed for cifar10 to be a similar classification difficulty for human a>
@@ -25,4 +21,8 @@ for i, noise_lvl in enumerate(noise_lvls):
     test_loader = torch.utils.data.DataLoader(test_data, batch_size=1, shuffle=False)
                              
     for _, (img, target) in enumerate(test_loader):
+        if i != 10 and i != 29: 
+            continue
+        if i > 29: 
+            break
         save_image(img, f"cifar10_{i}_{_}.png")
